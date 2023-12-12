6af8c19fe6e9c25dd88663ff38746f5ac2343d0f
RGuerra775 <84286263+RGuerra775@users.noreply.github.com>
2023-12-12 21:14:02 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-function showElement() {
+async function showElement() {
     element = document.querySelector('.h3a');
       element.style.visibility = 'visible';
+      await sleep(1000)
+      element.style.visibility = 'hidden'; 
 }
 
-function hideElement() {
-    element = document.querySelector('.h3a');
-      element.style.visibility = 'hidden';
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-      function validate() {
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
+function validate() {
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
     if ( username == "root" && password == "root") {
         alert ("Login successfully");
         window.open("https://rguerra775.github.io/Personal-website/main/index")
-        return false;
     }
     else{
-        showElement;
-        //await delay(2000);
-        //hideElement;
+        showElement();
     }
+}
+
+function show1Element() {
+    element = document.querySelector('.btn-login');
+        element.style.visibility = 'hidden'
+    element = document.querySelector('.login-div');
+      element.style.visibility = 'visible';
 }
